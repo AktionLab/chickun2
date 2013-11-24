48b4fc68eea07e845d042ae2b6e882339581adeb
Daniel Zukowski <daniel@aktionlab.com>
2013-11-24 14:38:20 -0700

    added more alts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,13 @@ Trade.delete_all
 [
   { exchange: Exchange.first, name: 'BTC/USD', key: 'btc_usd' }, 
   { exchange: Exchange.first, name: 'LTC/BTC', key: 'ltc_btc' },
-  { exchange: Exchange.first, name: 'LTC/USD', key: 'ltc_usd' } 
+  { exchange: Exchange.first, name: 'LTC/USD', key: 'ltc_usd' }, 
+  { exchange: Exchange.first, name: 'NMC/BTC', key: 'nmc_btc' },
+  { exchange: Exchange.first, name: 'NMC/USD', key: 'nmc_usd' }, 
+  { exchange: Exchange.first, name: 'NVC/USD', key: 'nvc_usd' }, 
+  { exchange: Exchange.first, name: 'NVC/BTC', key: 'nvc_btc' }, 
+  { exchange: Exchange.first, name: 'TRC/BTC', key: 'trc_btc' }, 
+  { exchange: Exchange.first, name: 'PPC/BTC', key: 'ppc_btc' }, 
+  { exchange: Exchange.first, name: 'FTC/BTC', key: 'ftc_btc' },	 
+  { exchange: Exchange.first, name: 'XPM/BTC', key: 'xpm_btc' } 
 ].each { |cp| CurrencyPair.create! cp }
